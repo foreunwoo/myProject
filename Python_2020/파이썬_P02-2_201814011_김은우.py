4cbf442f698153bd26b634d0b8a6ce463aa4e9b9
김은우 <agasiya99@gmail.com>
2020-04-25 00:35:35 +0900

    [UPDATE] Modify comment

--- Python_2020/파이썬_P02-2_201814011_김은우.py
+++ Python_2020/파이썬_P02-2_201814011_김은우.py
@@ -1,10 +1,10 @@
 sum=0
-answer=input("sum 프로그램을 시작하시겠습니까? ") // y 입력시 프로그램 시작
+answer=input("sum 프로그램을 시작하시겠습니까? ") # y 입력시 프로그램 시작
 
-while answer=='y' or answer=='Y': // 대소문자 둘 다 가능하게
+while answer=='y' or answer=='Y': # 대소문자 둘 다 가능하게
     n=int(input("숫자를 입력하세요: "))
     sum += n
     answer=input("sum 프로그램을 계속하시겠습니까?(Y/N) ")   
-    if answer=='n' or answer=='N': // n 입력시 총합 출력 후 반복문을 빠져나옴
+    if answer=='n' or answer=='N': # n 입력시 총합 출력 후 반복문을 빠져나옴
         print("합은",sum)
         break
