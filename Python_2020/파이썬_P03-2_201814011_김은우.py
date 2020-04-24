95061394f9e0093b8b68c63dcea0d1f5ec3cfb63
김은우 <agasiya99@gmail.com>
2020-04-25 00:34:25 +0900

    [UPDATE] Modify comment

--- Python_2020/파이썬_P03-2_201814011_김은우.py
+++ Python_2020/파이썬_P03-2_201814011_김은우.py
@@ -10,7 +10,7 @@ def multiply(a,b):
 def divide(a,b):
     return a / b
 
-def calculation(a,b,cal): // 연산 입력 시 실행될 메소드
+def calculation(a,b,cal): # 연산 입력 시 실행될 메소드
     intValue = 0
 
     if cal ==  "1":
@@ -28,7 +28,7 @@ def main():
     print('=== 1:덧셈 2:뺄셈 3:곱셈 4:나눗셈 0:종료 ===')
     while True:
         cal = input("연산 입력: ")
-        if cal == "0": // 0 입력 시 루프를 빠져나옴
+        if cal == "0": # 0 입력 시 루프를 빠져나옴
             print('연산 종료')
             break
         a = float(input("정수 입력: "))
